6ef03495a0b008414d5d39f9ede98bc95b53f972
Rafael <88090582+rafa-san@users.noreply.github.com>
2024-08-12 16:17:34 -0300

    Alteração do nome do arquivo

--- gradebook-app-meu-codigo.js
+++ gradebook-app-meu-codigo.js
@@ -11,19 +11,6 @@ function getAverage(scores) {
 //console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 //console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
 
-/*
-Solução freeCodeCamp
-function getAverage(scores) {
-  let sum = 0;
-
-  for (const score of scores) {
-    sum += score;
-  }
-
-  return sum / scores.length;
-}
-*/
-
 
 // Step 2
 function getGrade(score) {
@@ -48,25 +35,7 @@ function getGrade(score) {
 //console.log(getGrade(82));
 //console.log(getGrade(56));
 
-/*
-Soluçao freeCodeCamp
 
-function getGrade(score) {
-  if (score === 100) {
-    return "A++";
-  } else if (score >= 90) {
-    return "A";
-  } else if (score >= 80) {
-    return "B";
-  } else if (score >= 70) {
-    return "C";
-  } else if (score >= 60) {
-    return "D";
-  } else {
-    return "F";
-  }
-}
-*/
 
 
 // Step 3
@@ -82,14 +51,6 @@ console.log(hasPassingGrade(100));
 console.log(hasPassingGrade(53));
 console.log(hasPassingGrade(87));
 
-
-/*
-Solução freeCodeCamp
-
-function hasPassingGrade(score) {
-  return getGrade(score) !== "F";
-}
-*/
 
 
 // Step 4
