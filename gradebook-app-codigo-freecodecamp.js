dd1eb107d1d681bc1afeab3a9d4230e0ca52c0ab
Rafael <88090582+rafa-san@users.noreply.github.com>
2024-08-12 16:18:10 -0300

    Criação do script com o código original

---
+++ gradebook-app-codigo-freecodecamp.js
@@ -0,0 +1,40 @@
+/*
+Solução freeCodeCamp
+function getAverage(scores) {
+  let sum = 0;
+
+  for (const score of scores) {
+    sum += score;
+  }
+
+  return sum / scores.length;
+}
+*/
+
+/*
+Soluçao freeCodeCamp
+
+function getGrade(score) {
+  if (score === 100) {
+    return "A++";
+  } else if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+*/
+
+/*
+Solução freeCodeCamp
+
+function hasPassingGrade(score) {
+  return getGrade(score) !== "F";
+}
+*/
